31d8eb1decc13491c18d4c8788d903a1da23020f
Abhishek kanojia <abhishekka3193@gmail.com>
2017-05-12 11:35:39 +0530

    Initialised Repo

---
+++ fadeInOut.js
@@ -0,0 +1,46 @@
+/*
+	I don't comment usually.
+	but let me make it easy for you
+	to use FadeInOut.js
+
+ */
+
+/*
+--------------
+	USAGE
+--------------	
+
+	$(document).ready(function(){
+
+		$(".fader").fadeInOut({
+			timeInterval: 3000,
+			brag: ['I am a Freelance Developer',
+					'A Hobbyist',
+					'Out of Box Thinker',
+					'And You\'re',
+					'An Awesome Visitor'
+				  ]
+		});
+	});
+----------------
+Yes, That's It.
+----------------
+
+*/
+
+jQuery.fn.extend({
+	fadeInOut: function(options) {
+		var element = $(this);
+		var i = 0;
+
+		setInterval(function(){
+			element.fadeOut("slow",function(){
+				if(i===options.brag.length){
+					i=0;
+				}
+				element.text(options.brag[i++]);
+				element.fadeIn("slow");
+			});
+		},options.timeInterval);
+	}
+});
